06ed4264c7bffee9875415d5ded36528784eac4b
Alexandr B <borisagh@yandex.ru>
2025-04-14 13:48:43 +0300

    Добавлены настройки подключения к БД

---
+++ db/config.py
@@ -0,0 +1,35 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class DatabaseSettings(BaseSettings):
+    # Параметры подключения
+    HOST: str
+    PORT: int
+    USER: str
+    PASSWORD: str
+    NAME: str
+    SCHEMA: str
+
+    # Настройки пула соединений
+    POOL_MIN: int = 10
+    POOL_MAX: int = 10
+    POOL_TIMEOUT: int = 30
+
+    @property
+    def dns(self) -> str:
+        return f"postgresql://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}/{self.NAME}"
+
+
+    model_config = SettingsConfigDict(
+        env_file="../.env",
+        env_file_encoding="utf-8",
+        env_prefix="DB_",
+        case_sensitive=False,
+        extra="ignore"
+    )
+
+
+settings = DatabaseSettings()
+
+if __name__ == "__main__":
+    print(settings.dns)
