d6972879c8cbc1781b855635970f5b85374a7c1e
Alexandr B <borisagh@yandex.ru>
2025-04-18 12:29:06 +0300

    Добавление хеширования и верификации пароля

---
+++ app/core/security.py
@@ -0,0 +1,15 @@
+from passlib.hash import bcrypt
+from passlib.exc import PasswordTruncateError
+
+def hash_password(password: str) -> str:
+    return bcrypt.hash(password)
+
+
+def verify_password_hash(plain_password, hashed_password):
+    return bcrypt.verify(plain_password, hashed_password)
+
+
+
+
+if __name__ == "__main__":
+    pass
