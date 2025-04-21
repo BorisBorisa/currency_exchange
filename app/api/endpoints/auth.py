b96b546528040a5b59620294efadd15d548938fe
Alexandr B <borisagh@yandex.ru>
2025-04-21 19:42:33 +0300

    Логика проверки авторизованного пользователя перенесена из auth в security

--- app/api/endpoints/auth.py
+++ app/api/endpoints/auth.py
@@ -1,17 +1,15 @@
-from fastapi import APIRouter, Depends, HTTPException, status, Query
+from fastapi import APIRouter, Depends, HTTPException, status
 from typing import Annotated
-from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordRequestForm
 from asyncpg import Connection
-from passlib.exc import InvalidTokenError
+
 
 from db.db_connecion import get_database_connection
 from db.queries import get_user_by_username
-from app.api.schemas.user import Token, UserInDB, User
-from app.core.security import verify_password_hash, create_access_token, decode_access_token
+from app.api.schemas.user import Token, UserInDB
+from app.core.security import  create_access_token, verify_password_hash
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 auth = APIRouter()
-
 
 async def authenticate_user(
         username: str,
@@ -29,40 +27,7 @@ async def authenticate_user(
     return user
 
 
-async def get_current_user(
-        token: Annotated[str, Depends(oauth2_scheme)],
-        conn: Connection = Depends(get_database_connection),
-) -> UserInDB:
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Не удалось подтвердить учетные данные",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-
-    try:
-        payload: dict = decode_access_token(token)
-        username = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-
-    except InvalidTokenError as exc:
-        raise credentials_exception
-
-    user = await get_user_by_username(conn, username)
-
-    return user
-
-
-async def get_current_active_user(
-        current_user: Annotated[UserInDB, Depends(get_current_user)],
-):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
-
-    return current_user
-
-
-@auth.post("/login", summary="эндпоинт для получения токена")
+@auth.post("/login", summary="эндпоинт для получения токена", tags=["tokenUrl"])
 async def login_for_access_token(
         from_data: Annotated[OAuth2PasswordRequestForm, Depends()],
         conn: Connection = Depends(get_database_connection)
@@ -78,10 +43,3 @@ async def login_for_access_token(
 
     access_token = create_access_token(data={"sub": from_data.username})
     return Token(access_token=access_token, token_type="bearer")
-
-
-@auth.get("/users/me/", response_model=User)
-async def read_users_me(
-        current_user: Annotated[User, Depends(get_current_active_user)],
-):
-    return current_user
--- app/core/security.py
+++ app/core/security.py
@@ -1,12 +1,18 @@
 import jwt
-from jwt import InvalidTokenError
+from jwt.exceptions import InvalidTokenError
 
-from fastapi import Depends
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+
 from datetime import datetime, timedelta, timezone
 from passlib.hash import bcrypt
+from typing import Annotated
+from asyncpg import Connection
 
 from app.core.config import jwt_settings
 from app.api.schemas.user import UserInDB
+from db.db_connecion import get_database_connection
+from db.queries import get_user_by_username
 
 
 def hash_password(password: str) -> str:
@@ -36,10 +42,41 @@ def decode_access_token(token: str):
     return jwt.decode(token, jwt_settings.SECRET_KEY, algorithms=[jwt_settings.ALGORITHM])
 
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+
+async def get_current_user(
+        token: Annotated[str, Depends(oauth2_scheme)],
+        conn: Connection = Depends(get_database_connection),
+) -> UserInDB:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Не удалось подтвердить учетные данные",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        payload: dict = decode_access_token(token)
+        username = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+
+    except InvalidTokenError as exc:
+        raise credentials_exception
+
+    user = await get_user_by_username(conn, username)
+
+    return user
+
+
+async def get_current_active_user(
+        current_user: Annotated[UserInDB, Depends(get_current_user)],
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+
+    return current_user
+
+
 if __name__ == "__main__":
-    a = hash_password("qwerty123")
-
-    assert verify_password_hash("qwerty123", a)
-    assert verify_password_hash("qwesrty123", a) == False
-
-    print(create_access_token(data={"useername": "oleg"}, expires_delta=timedelta(minutes=30)))
+    pass
